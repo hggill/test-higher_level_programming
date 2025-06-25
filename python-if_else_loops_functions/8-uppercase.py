a485888711dfef2b8e55b2687893a37f5862e4f4
hggill <Harrygill@grosvenor.com.au>
2025-06-25 14:05:12 +1000

    8. To uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Write a function that prints a string in uppercase followed by a new line.
+def uppercase(str):
+    result = ""
+    for c in str:
+        if ord(c) in range(97, 123):
+            result += chr(ord(c) - 32)
+        else:
+            result += c
+    print('{}'.format(result))
+
+
+uppercase("Best School 98 Battery street")
