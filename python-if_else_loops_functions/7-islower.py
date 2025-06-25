c8c56bf9a305691e07bb759c9ff7a4d7b2bf3774
hggill <Harrygill@grosvenor.com.au>
2025-06-25 12:40:12 +1000

    7. islower

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def islower(c):
-    if ord(c) in range(97, 123):
-        print(f'{c} is lower')
-    elif ord(c) in range(65, 91):
-        print(f'{c} is upper')
+    return ord(c) in range(97, 123)
