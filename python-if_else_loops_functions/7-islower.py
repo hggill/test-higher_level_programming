8d5886f002783769623dd7261159a26a0b7dd9ad
hggill <Harrygill@grosvenor.com.au>
2025-06-25 12:27:28 +1000

    7. islower

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -4,9 +4,3 @@ def islower(c):
         print(f'{c} is lower')
     elif ord(c) in range(65, 91):
         print(f'{c} is upper')
-    else:
-        print(f'{c} is not a letter')
-
-
-for i in range(65, 123):
-    islower(chr(i))
