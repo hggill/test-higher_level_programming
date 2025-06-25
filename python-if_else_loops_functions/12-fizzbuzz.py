d1e42fbbefbe0b0290691825105473e06cf7388a
hggill <Harrygill@grosvenor.com.au>
2025-06-25 15:53:23 +1000

    12. Fizz Buzz

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,10 +2,8 @@
 # Write a function that prints the numbers from 1 to 100 separated by a space.
 def fizzbuzz():
     result = ''
-    for i in range(0, 101):
-        if i == 0:
-            result += f'{i} '
-        elif i % 3 == 0 and i % 5 == 0:
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
             result += "FizzBuzz "
         elif i % 3 == 0:
             result += "Fizz "
