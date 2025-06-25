707fc675fcd1c03709c2d0d1d7bdd67f19057f72
hggill <Harrygill@grosvenor.com.au>
2025-06-25 14:30:00 +1000

    9. There are only 3 colors

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 # Write a function that prints the last digit of a number
 def print_last_digit(number):
-    print(number[len(number)-1])
+    num = str(number)
+    print(num[len(num)-1])
